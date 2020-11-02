be854e786fdfcd3cd4931ffc727336dbf812542b
Isabel Lee <isabel@pathrise.com>
2020-11-02 02:43:28 -0800

    use rack cors

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.6.4'
 gem 'rails', '~> 6.0.2'
 gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 4.1'
+gem 'rack-cors'
 
 gem 'will_paginate', '~> 3.1.0'
 
--- app/controllers/api/crunchbase_companies_controller.rb
+++ app/controllers/api/crunchbase_companies_controller.rb
@@ -1,6 +1,4 @@
 class Api::CrunchbaseCompaniesController < ApplicationController
-  before_action :allow_cross_origin
-
   def index
     render json: { crunchbase_companies: paginate(CrunchbaseCompany.all) }
   end
@@ -9,13 +7,5 @@ class Api::CrunchbaseCompaniesController < ApplicationController
     name = params[:name]
     company = CrunchbaseCompany.find_by(name: name)
     render json: { company: company }
-  end
-
-  private
-
-  def allow_cross_origin
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    response.headers["Access-Control-Allow-Headers"] = "accept, content-type"
   end
 end
